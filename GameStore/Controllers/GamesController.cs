219e9b96530a13ec0436b27f9425d3b2f550c502
Fabricio Batista Pinheiro <fabriciobpinheiro@gmail.com>
2024-05-19 23:40:39 -0300

    -> Renamed Attribute

--- GameStore/Controllers/GamesController.cs
+++ GameStore/Controllers/GamesController.cs
@@ -9,19 +9,19 @@ namespace GameStore.Controllers
     [Route("[controller]")]
     public class GamesController : ControllerBase
     {
-        private readonly ILogger<GamesController> _logger;
+        private readonly ILogger<GamesController> _Logger;
         private readonly IGameService _GameService;
 
         public GamesController(ILogger<GamesController> logger, IGameService gameService)
         {
-            _logger = logger;
+            _Logger = logger;
             _GameService = gameService;
         }
 
         [HttpPost()]
         public ActionResult<Game> AddGame([FromBody] Game game)
         {
-            _logger.LogDebug("The API was called");
+            _Logger.LogDebug("The API was called");
             _GameService.AddGame(game);
             return Created();
         }
@@ -30,7 +30,7 @@ namespace GameStore.Controllers
         [HttpGet()]
         public ActionResult<IEnumerable<Game>> GetAllGames()
         {
-            _logger.LogDebug("The API was called");
+            _Logger.LogDebug("The API was called");
             var result = _GameService.GetAllGames();
            return Ok(result);
         }
