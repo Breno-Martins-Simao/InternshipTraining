3206167f8bf4fab73568153dcfd88c4b5ccd9411
Breno Martins Simão <brenomsimao123@outlook.com>
2024-05-26 20:53:26 -0300

    -> Created Game model and add EF Packages

---
+++ GameStore/Database/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.Models.Database;
+using System.Data.Entity;
+
+namespace GameStore.Database
+{
+    public class GamesDb : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ GameStore/Models/Database/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace GameStore.Models.Database
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Platform { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+    }
+}
